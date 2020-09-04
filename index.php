a252568f98d8db9d450f21c66189200fd8b0f70b
geeksgod <bipinmaharjan750@gmail.com>
2020-09-04 15:59:39 +0545

    extentreport

--- index.php
+++
@@ -1,4 +0,0 @@
-<form method="POST" enctype="multipart/form-data" action="form.php">
-        Select document to upload (only pdf are allowed): <input type="file" name="document">
-        <input type="submit" value="Upload Document" name="submit">
-</form>
