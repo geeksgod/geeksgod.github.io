22dc44d877cc21c2ac625eb138675ae430d78cf5
geeksgod <bipinmaharjan750@gmail.com>
2020-09-04 10:45:12 +0545

    test

---
+++ index.php
@@ -0,0 +1,4 @@
+<form method="POST" enctype="multipart/form-data" action="form.php">
+        Select document to upload (only pdf are allowed): <input type="file" name="document">
+        <input type="submit" value="Upload Document" name="submit">
+</form>
